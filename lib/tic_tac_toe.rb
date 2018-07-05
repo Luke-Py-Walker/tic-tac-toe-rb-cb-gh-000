bbb81b8324c5a52226fab405b86b538711724cec
Lucas Wang <lucas211w@gmail.com>
2018-07-05 23:16:35 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,7 +11,7 @@ def turn(board)
   user_input = gets.chomp
   index = input_to_index(user_input)
   if valid_move?(board,index)
-    move(board,index)
+    move(board,index,current_player(board))
     display_board(board)
   else
     turn(board)
